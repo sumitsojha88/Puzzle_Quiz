f1a0dc299401a862fed5f360db117ff6645fc242
sumitsojha88 <greatsojha88@gmail.com>
2021-02-23 14:52:26 +0530

    Home button Added in everyscreen

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:puzzle_riddle_apti_app/Login/login.dart';
+import 'package:puzzle_riddle_apti_app/menu_bar/sidebar_layout.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
       title: 'Quiz App',
       debugShowCheckedModeBanner: false,
       // theme: ThemeData.dark(),
-      home: Loginpage2(),
+      home: SideBarLayout(),
     );
   }
 }
