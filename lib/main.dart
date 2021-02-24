5e1d67fcf2ce3c7ce3ae9d4bb8d93d630af18925
Priyansh Jain <48293372+ErR0rpj@users.noreply.github.com>
2021-02-24 12:30:51 +0530

    User login check with bug

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:puzzle_riddle_apti_app/Login/login.dart';
+import 'package:puzzle_riddle_apti_app/menu_bar/sidebar_layout.dart';
+import 'package:puzzle_riddle_apti_app/screens/loading_screen.dart';
+import 'package:puzzle_riddle_apti_app/useful_compenents/global_variables.dart';
 
-void main() async{
+void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
@@ -13,11 +16,12 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+
     return GetMaterialApp(
       title: 'Quiz App',
       debugShowCheckedModeBanner: false,
       // theme: ThemeData.dark(),
-      home: Loginpage2(),
+      home: LoadingScreen(),
     );
   }
 }
