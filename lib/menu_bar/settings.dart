83b183e048f70bc8615b0f23abb5142f7828ebbb
sumit ojha <greatsojha88@gmail.com>
2021-02-25 01:46:19 +0530

    Change Password button

--- lib/menu_bar/settings.dart
+++ lib/menu_bar/settings.dart
@@ -3,18 +3,14 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:puzzle_riddle_apti_app/Category_puzzle_components/Category_puzzle_details.dart';
 import 'package:puzzle_riddle_apti_app/menu_bar/navigation.dart';
+import 'package:puzzle_riddle_apti_app/menu_bar/settingspage.dart';
 
 
 
 class SettingsPage extends StatelessWidget with NavigationStates {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        "My Settings",
-        style: TextStyle(fontWeight: FontWeight.w900, fontSize: 28),
-      ),
-    );
+    return SettingsPage2();
 
   }
 }
